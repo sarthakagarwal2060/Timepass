a40eee2481588e37169ad59c5f63acb6267ba459
sarthakagarwal2060 <sarthakagarwal6011@gmail.com>
2025-08-28 13:14:41 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import dotenv from "dotenv";
 import express from "express";
-import notesRoute from "./routes/notesRoute.js";
+import router  from "./routes/notesRoute.js";
 import dbConnect from "./config/dbConnect.js";
 dotenv.config();
 dbConnect()
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+// let notes = [
+//   {
+//     id: "1",
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: "2",
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: "3",
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
 const Port = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
-app.use("/notes", notesRoute);
+app.use("/notes", router);
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
