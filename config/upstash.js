4dda70cb186ed486053c301533eee575d1f14c3f
sarthakagarwal2060 <sarthakagarwal6011@gmail.com>
2025-09-09 19:20:56 +0530

    reddis removed

--- config/upstash.js
+++ config/upstash.js
@@ -1,11 +1,9 @@
-import { Redis } from "@upstash/redis";
-import {Ratelimit} from "@upstash/ratelimit"
-import dotenv from "dotenv"
-dotenv.config()
+import { Ratelimit } from "@upstash/ratelimit";
+import dotenv from "dotenv";
+dotenv.config();
 const ratelimit = new Ratelimit({
-    redis:Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(5,"10 s")
-
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
 });
 
-export default ratelimit
+export default ratelimit;
