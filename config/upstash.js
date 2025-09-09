4fcc27e99d42285812fd2255df7be75c6ad82826
sarthakagarwal2060 <sarthakagarwal6011@gmail.com>
2025-09-09 19:23:09 +0530

    commit

--- config/upstash.js
+++ config/upstash.js
@@ -1,9 +1,11 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import dotenv from "dotenv";
-dotenv.config();
+import { Redis } from "@upstash/redis";
+import {Ratelimit} from "@upstash/ratelimit"
+import dotenv from "dotenv"
+dotenv.config()
 const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
+    redis:Redis.fromEnv(),
+    limiter:Ratelimit.slidingWindow(5,"10 s")
+
 });
 
-export default ratelimit;
+export default ratelimit
